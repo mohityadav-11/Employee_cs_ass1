fee836b14e189149066d575bb078cf9bc629de1b
Mohit Yadav <Mohityadav6532@gmail.com>
2020-01-24 14:25:27 +0530

    Employee file added

---
+++ Employee/Employee/Employee.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    class Employee
+    {
+        private int id;
+        private string name;
+        private string deptName;
+
+        string _fire;
+
+        public delegate void Fired(String msg);
+        public event Fired OnFired;
+
+        public Employee(int id , string name , string deptName)
+        {
+            this.id = id;
+            this.name = name;
+            this.deptName = deptName;
+
+            this._fire = "Empty";
+        }
+
+
+        public int getID() {
+            OnFired("GetID() Method is called");
+            return this.id;
+        }
+
+        public string getName()
+        {
+            OnFired("GetName() Method is called");
+            return this.name;
+        }
+        
+        public string getDepartmentName()
+        {
+            OnFired("GetDepartmentName() Method is called");
+            return this.deptName;
+        }
+
+        public void update(int id)
+        {
+            OnFired("update() Method is called");
+            this.id = id;
+        }
+
+        public void update(String name)
+        {
+            OnFired("update() Method is called");
+            this.name = name;
+        }
+
+        public void  updateDepName(String dept)
+        {
+            OnFired("updateDepName() Method is called");
+            this.deptName = dept;
+        }
+    }
+}
---
+++ Employee/Employee/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Employee id");
+            int id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Employee Name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter Department Name");
+            string deptName = Console.ReadLine();
+
+            Employee e1 = new Employee(id, name, deptName);
+            e1.OnFired += new Employee.Fired(called);
+
+
+            Console.WriteLine("Employee details : ");
+            Console.WriteLine("Employee ID :- "+e1.getID());
+            Console.WriteLine("Employee Name :-" + e1.getName());
+            Console.WriteLine("Employee Department Name :- " + e1.getDepartmentName());
+
+            e1.update(420);
+            e1.update("Rohan");
+            e1.updateDepName("Account Dept");
+
+            Console.WriteLine("Updated Employee details : ");
+            Console.WriteLine("Employee ID :- " + e1.getID());
+            Console.WriteLine("Employee Name :-" + e1.getName());
+            Console.WriteLine("Employee Department Name :- " + e1.getDepartmentName());
+
+        }
+
+        static void called(string ss)
+        {
+            Console.WriteLine($"{ss}");
+        }
+    }
+}
